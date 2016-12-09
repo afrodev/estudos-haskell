d2204f4cd11ca2b5b30856346dfd95d9ec4129ef
Humberto Vieira <humberto@indice.in>
2016-12-09 16:11:37 -0200

    Esta com uma questao da prova do nelsu

---
+++ prova2/ques4_nelsu.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	int j = 0;
+	int i = 3/j;
+
+	for (i = 1; i > 1; i++)
+		i--;
+
+
+	return 0;
+}
+
+
+
+/*
+	QUESTAO 4
+		- DA FLOAT POINT EXCEPTION
+		
+
+
+*/
