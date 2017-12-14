93eb8e814b36347222899e062756f5feb799c762
Humberto Vieira <humbertovieira.castro@gmail.com>
2017-12-14 15:06:02 -0200

    Update all-haskell-exercises.hs

--- all-haskell-exercises.hs
+++ all-haskell-exercises.hs
@@ -1,92 +1,98 @@
-e_primo(2). 
-e_primo(3). 
-e_primo(P) :- integer(P), P > 3, P mod 2 =\= 0, \+tem_factor(P,3). 
-tem_factor(N,L) :- N mod L =:= 0. 
-tem_factor(N,L) :- L * L < N, L2 is L + 2, tem_factor(N,L2). 
+
+-- merges --
+
+quicksort [] = []
+quicksort (x:xs) = quicksort small ++ (x : quicksort large)
+   where small = [y | y <- xs, y <= x]
+         large = [y | y <- xs, y > x]
 
 
-fibonacci(0,1). 
-fibonacci(1,1). 
-fibonacci(N,F):-   
-N > 1, 
-N1 is N - 1, fibonacci(N1,F1), 
-N2 is N - 2, fibonacci(N2,F2), 
-F is F1 + F2.   
+-- lista 2--
+
+bissexto :: Int -> Bool
+bissexto ano | mod ano 4 == 0 && (mod ano 100 /= 0 || mod ano 400 == 0) = True
+			 | otherwise = False
 
 
-mdc(X,0,Z) :- !, Z = X.
-mdc(X,Y,Z) :- R is X mod Y, mdc(Y,R,Z).
+isSquare :: Int -> Bool
+isSquare n = truncate(sqrt(x)) * truncate(sqrt(x)) == n
+             where x = fromIntegral n
 
 
-%Casos base: P(0) = P(1) = P(2) = 1
-padovan(0,1).
-padovan(1,1).
-padovan(2,1).
-%Caso recursivo
-padovan(N,P) :-
-	N =\= 0,
-	N =\= 1,
-	N =\= 2,
-	N1 is N - 2,
-	N2 is N - 3,
-	padovan(N1,P1),
-	padovan(N2,P2),
-	P is P1 + P2.
+mdc :: Int -> Int -> Int
+mdc m n | m == 0  = n
+		| m > 0  = mdc ( n `mod` m) m   
 
 
-perfeito(N) :- soma_div(N, N).
+collatz n
+     | mod n 2 == 0 = div n 2
+     | otherwise =  3 * n + 1
 
-%%% soma_div(N, S) eh satisfeito quando S eh a soma dos divisores de N menores que ele proprio
-soma_div(N, S) :- N1 is N - 1, soma_div_ateh(N, N1, S).
-
-%%% soma_div_ateh(N, L, S) eh satisfeito quando S eh a soma dos dividores de N ateh L
-%%% (ou seja, dividores menores ou iguais a L)
-soma_div_ateh(_, 0, 0) :- !.
-soma_div_ateh(N, L, S) :-
-    L1 is L - 1,
-    soma_div_ateh(N, L1, S1),
-    parcela(N, L, P),
-    S is S1 + P.
-
-%%% parcela(N, L, P) eh satisfeito quando P eh a parcela a somar nos dividores de N relativa a L.
-%%% em outras palavras, se L divide N, a parcela eh L; se L nao divide N, a parcela eh zero.
-parcela(N, L, 0) :- 0 =\= N mod L.
-parcela(N, L, L) :- 0 =:= N mod L.
+collatzList :: Int -> [Int]
+collatzList n | n < 1 = error "Cannot have negative number"
+				| n == 1 = [n]
+				| otherwise = n:collatzList (collatz n)
 
 
-is_sublist([], []).
-is_sublist([_|_] ,[]).
-is_sublist([H|T], L) :-
-    once(select(H, L, L2)),
-    is_sublist(L2, T).
+-- lista 3--
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery list count = helper list count count
+  where helper [] _ _ = []
+        helper (x:xs) count 1 = helper xs count count
+        helper (x:xs) count n = x : (helper xs count (n - 1))
 
 
-merge_sort([],[]).     % empty list is already sorted
-merge_sort([X],[X]).   % single element list is already sorted
-merge_sort(List,Sorted):-
-    List=[_,_|_],divide(List,L1,L2),     % list with at least two elements is divided into two parts
-	merge_sort(L1,Sorted1),merge_sort(L2,Sorted2),  % then each part is sorted
-	merge(Sorted1,Sorted2,Sorted).                  % and sorted parts are merged
-merge([],L,L).
-merge(L,[],L):-L\=[].
-merge([X|T1],[Y|T2],[X|T]):-X=<Y,merge(T1,[Y|T2],T).
-merge([X|T1],[Y|T2],[Y|T]):-X>Y,merge([X|T1],T2,T).    
+
+rotate xs n | n >= 0 = drop n xs ++ take n xs
+            | n < 0 = drop len xs ++ take len xs
+                      where len = n+length xs
+
+-- chic --
+
+digitos :: Int -> Int
+digitos n
+		| n >= 10 = digitos ((quot n 10) + (mod n 10))
+		| otherwise = n
 
 
-quick_sort([],[]).
-quick_sort([H|T],Sorted):-
-	pivoting(H,T,L1,L2),quick_sort(L1,Sorted1),quick_sort(L2,Sorted2),
-	append(Sorted1,[H|Sorted2]).
-   
-pivoting(H,[],[],[]).
-pivoting(H,[X|T],[X|L],G):-X=<H,pivoting(H,T,L,G).
-pivoting(H,[X|T],L,[X|G]):-X>H,pivoting(H,T,L,G).
+contem :: Int -> Int -> Bool
+contem n x
+	| n < 10 = n == x
+	| (mod n 10) == x = True
+	| otherwise = contem (quot n 10) x
+
+chic :: Int -> Bool
+chic n = contem n (digitos n)
 
 
-bubble_sort(List,Sorted):-b_sort(List,[],Sorted).
-b_sort([],Acc,Acc).
-b_sort([H|T],Acc,Sorted):-bubble(H,T,NT,Max),b_sort(NT,[Max|Acc],Sorted).
-   
-bubble(X,[],[],X).
-bubble(X,[Y|T],[Y|NT],Max):-X>Y,bubble(X,T,NT,Max).
-bubble(X,[Y|T],[X|NT],Max):-X=<Y,bubble(Y,T,NT,Max).
+
+-- lista 4--
+
+goldbach a = head $
+                     filter (\(x,y) -> isPrime x && isPrime y) $
+                     map (\e -> (e, a - e)) [1,3..a `div` 2]
+ where
+ factors a = filter (isFactor a) [2..a-1]
+ isFactor a b = a `mod` b == 0
+ isPrime a = null $ factors a
+
+mergesort [] = [] 
+mergesort [a] = [a]
+mergesort a = let pulka = length a `div` 2 in merge (mergesort $ take pulka a) (mergesort $ drop pulka a) 
+
+merge a [] = a
+merge [] a = a
+merge (a:ra) (b:rb) = if a > b then ([b] ++ (merge (a:ra) (rb))) else ([a] ++ (merge (ra) (b:rb)))
+
+
+fatorial n
+		| n == 0 = 1
+		| n > 0 = n * fatorial(n - 1)
+
+
+cosseno :: Double ->  Double
+cosseno n = sum [((-1) ** x) / (fatorial (2 * x)) | x <- [0..n]]
+
+
+--(\x y -> x^2 + y^2) 3 5
